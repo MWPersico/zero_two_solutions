979c9705eb369b3620ac1036a7df626c937df192
MWPersico <willian.marcos7@hotmail.com>
2024-11-11 19:59:01 -0300

    backend project created

---
+++ backend/src/main/java/br/senac/ead/zero_two_solutions/ZeroTwoSolutionsApplication.java
@@ -0,0 +1,13 @@
+package br.senac.ead.zero_two_solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroTwoSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroTwoSolutionsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/senac/ead/zero_two_solutions/ZeroTwoSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.ead.zero_two_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroTwoSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
